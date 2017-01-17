dd9f0d2f86df67b92dccc1a9c708a83541f1a889
Darshan Shaligram <darshan.shaligram@hobsons.com>
2017-01-17 12:23:26 -0500

    Add GOPATH special case, unhardcode

--- pathabbrev.go
+++ pathabbrev.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -11,16 +12,62 @@ import (
 const homeSigil = "~"
 const pathSeparator = string(os.PathSeparator)
 
-var rootFiles = []string{".git", ".hg", ".svn", "pom.xml", "package.json", ".editorconfig"}
-var shortenRegex = regexp.MustCompile(`^([\w-]).*`)
+var rootFiles = flag.String("root-files", ".git,.hg,.svn,pom.xml,package.json,.editorconfig", "Files/directories that indicate a repository root when present")
+var magicEnv = flag.String("magic-root", "GOPATH", "Environment variables defining special directory paths that should be abbreviated as $ENV")
 
-func fileExists(file string) bool {
-	_, err := os.Stat(file)
-	return err == nil
+type env struct {
+	name  string
+	value string
 }
 
-func sourceRoot(dir string) bool {
-	for _, file := range rootFiles {
+type envrange []env
+
+// EnvPrefix returns an environment prefix that applies to path, and how many
+// leading path segments that prefix replaces.
+func (e envrange) EnvPrefix(path string) (prefix string, replacedSegments int) {
+	for _, envRep := range e {
+		if path == envRep.value || strings.HasPrefix(path, envRep.value+pathSeparator) {
+			return envRep.name, len(strings.Split(envRep.value, pathSeparator))
+		}
+	}
+	return "", 0
+}
+
+type pathShortener struct {
+	rootFiles []string
+	envs      envrange
+}
+
+func stripTrailingSlash(dir string) string {
+	if len(dir) <= 1 {
+		return dir
+	}
+	return strings.TrimSuffix(dir, pathSeparator)
+}
+
+func getEnvs(envNames []string) (envs envrange) {
+	for _, envName := range envNames {
+		envVar := strings.TrimSpace(envName)
+		if envVar == "" {
+			continue
+		}
+
+		if envValue := os.Getenv(envName); envValue != "" {
+			envs = append(envs, env{name: "$" + envName, value: stripTrailingSlash(envValue)})
+		}
+	}
+	return append(envs, env{name: homeSigil, value: os.Getenv("HOME")})
+}
+
+func newPathShortener(rootFiles, envNames []string) pathShortener {
+	return pathShortener{
+		rootFiles: rootFiles,
+		envs:      getEnvs(envNames),
+	}
+}
+
+func (p pathShortener) sourceRoot(dir string) bool {
+	for _, file := range p.rootFiles {
 		if fileExists(filepath.Join(dir, file)) {
 			return true
 		}
@@ -28,54 +75,78 @@ func sourceRoot(dir string) bool {
 	return false
 }
 
-func shorten(pathSegment string) string {
+func (p pathShortener) shorten(pathSegment string) string {
 	return shortenRegex.ReplaceAllString(pathSegment, "$1")
 }
 
-func inDir(path, parent string) bool {
-	return strings.HasPrefix(path, parent+"/")
-}
-
-func abbrev(path, homeDir string) string {
-	if path == homeDir {
-		return homeSigil
+// Shorten shortens a path applying rootEnvs and rootFiles
+func (p pathShortener) Shorten(path string) string {
+	if path == "" {
+		return ""
 	}
 
-	inHome := inDir(path, homeDir)
-	start := 0
-	if inHome {
-		start = len(strings.Split(homeDir, pathSeparator))
+	prefix, start := p.envs.EnvPrefix(path)
+
+	var shortenedSegments []string
+	add := func(segment string) {
+		shortenedSegments = append(shortenedSegments, segment)
 	}
 
-	segments := strings.Split(path, string(os.PathSeparator))
+	if prefix != "" {
+		add(prefix)
+	}
+
+	segments := strings.Split(path, pathSeparator)
 	endSegment := len(segments) - 1
 
-	var pathSegments []string
-
-	add := func(segment string) {
-		pathSegments = append(pathSegments, segment)
-	}
-
-	if inHome {
-		add(homeSigil)
+	shortenedSegment := func(i int) string {
+		dir := strings.Join(segments[:i+1], pathSeparator)
+		if p.sourceRoot(dir) {
+			return segments[i]
+		}
+		return p.shorten(segments[i])
 	}
 
 	for i := start; i < endSegment; i++ {
-		dir := strings.Join(segments[:i+1], pathSeparator)
+		add(shortenedSegment(i))
+	}
 
-		if sourceRoot(dir) {
-			add(segments[i])
-		} else {
-			add(shorten(segments[i]))
+	if endSegment >= start {
+		add(segments[endSegment])
+	}
+
+	return strings.Join(shortenedSegments, pathSeparator)
+}
+
+var shortenRegex = regexp.MustCompile(`^([\w-]).*`)
+
+func fileExists(file string) bool {
+	_, err := os.Stat(file)
+	return err == nil
+}
+
+func split(s string) []string {
+	inputSegments := strings.Split(s, ",")
+	trimmedStrings := make([]string, 0, len(inputSegments))
+	for _, part := range inputSegments {
+		if trimmed := strings.TrimSpace(part); trimmed != "" {
+			trimmedStrings = append(trimmedStrings, trimmed)
 		}
 	}
-	add(segments[len(segments)-1])
-
-	return strings.Join(pathSegments, pathSeparator)
+	return trimmedStrings
 }
 
 func main() {
-	for _, path := range os.Args[1:] {
-		fmt.Println(abbrev(path, os.Getenv("HOME")))
+	flag.Parse()
+
+	if len(flag.Args()) == 0 {
+		fmt.Fprintln(os.Stderr, "Usage:", os.Args[0], "[options] dir-name1 [dir-name2 ...]")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	shortener := newPathShortener(split(*rootFiles), split(*magicEnv))
+	for _, path := range flag.Args() {
+		fmt.Println(shortener.Shorten(path))
 	}
 }
